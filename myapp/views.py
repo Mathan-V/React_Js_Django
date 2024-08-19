1fa68c68ea800520cb45f56996ad4adb6e8739c0
Mathan-V <marimathan1998@gmail.com>
2024-08-19 11:39:37 +0530

    form updated

---
+++ myapp/views.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from .models import UserData
+from .serializers import UserDataSerializer
+
+class UserDataViewSet(viewsets.ModelViewSet):
+    queryset = UserData.objects.all()
+    serializer_class = UserDataSerializer
